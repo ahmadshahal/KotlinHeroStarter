fb24411be0a33700c6980e6b4d2a98bc4926ebd2
KotlinHero <ahmad.alshahal2@gmail.com>
2025-01-13 15:38:23 +0300

    test: LanguageViewModelTest

---
+++ app/src/test/java/com/kotlinhero/starter/viewmodels/LanguageViewModelTest.kt
@@ -0,0 +1,39 @@
+package com.kotlinhero.starter.viewmodels
+
+import com.kotlinhero.starter.core.foundation.domain.enums.Language
+import com.kotlinhero.starter.core.foundation.utils.localization.LocalizationUtils
+import com.kotlinhero.starter.features.settings.presentation.viewmodels.LanguageViewModel
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class LanguageViewModelTest {
+
+    private lateinit var languageViewModel: LanguageViewModel
+
+    @Before
+    fun setup() {
+        languageViewModel = LanguageViewModel()
+    }
+
+    @Test
+    fun `test onChangeLanguage to arabic, language should be changed correctly`() = runTest {
+        // When
+        languageViewModel.onLanguageChange(Language.Arabic)
+
+        // Then
+        Assert.assertTrue(languageViewModel.state.value.selectedLanguage == Language.Arabic)
+        Assert.assertTrue(LocalizationUtils.getSelectedLanguage() == Language.Arabic)
+    }
+
+    @Test
+    fun `test onChangeLanguage to english, language should be changed correctly`() = runTest {
+        // When
+        languageViewModel.onLanguageChange(Language.English)
+
+        // Then
+        Assert.assertTrue(languageViewModel.state.value.selectedLanguage == Language.English)
+        Assert.assertTrue(LocalizationUtils.getSelectedLanguage() == Language.English)
+    }
+}
